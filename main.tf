9c18fb33c019f67079ab58545cba498017a5ca45
enjuto <javier.sanzenjuto@gmail.com>
2017-08-27 18:58:53 +0200

    Create network, subnetwork instance and secret file

--- main.tf
+++ main.tf
@@ -1,19 +1,4 @@
-// Necessary items to perform initial infrastructure bootstrap
-
 // Variables
-//variable "gw" {}
-//variable "os_region" { default = "RegionOne" }
-//variable "deployment" { }
-//variable "dc" { }
-//variable "pool" {default = "DEV"}
-//variable "dns_server" { type = "list"}
-//variable "dns_domain" { default = "iaas.igrupobbva" }
-//variable "image" { }
-//variable "extattr" { }
-//variable "nomad_region" { }
-//variable "consul_dc" { }
-//variable "bootstrap_flavor_name" { default = "m1.small" }
-
 variable "env" {default = "lab1"}
 
 // Providers
@@ -21,4 +6,34 @@ provider "google" {
   credentials = "${file("secrets/account.json")}"
   project     = "328846467581"
   region      = "europe-west1"
+}
+
+// Networks
+resource "google_compute_network" "default" {
+  name = "default-${var.env}"
+}
+
+//Subnetworks
+
+resource "google_compute_subnetwork" "default" {
+  name   = "default-${var.env}"
+  region = "europe-west1"
+  ip_cidr_range = "10.0.0.0/24"
+  network = "${google_compute_network.default.name}"
+}
+
+//Instance
+
+resource "google_compute_instance" "default-img" {
+  name         = "default-img-${var.env}"
+  machine_type = "f1-micro"
+  zone         = "europe-west1-b"
+  tags = ["default"]
+  disk {
+    image = "centos-cloud/centos-7"
+  }
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.default.name}"
+  //  access_config = "${google_compute_address.default.name}"
+  }
 }
