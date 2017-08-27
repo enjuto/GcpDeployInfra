8693439cd97e87ac46aff88d6d831beb47179587
enjuto <javier.sanzenjuto@gmail.com>
2017-08-27 19:34:20 +0200

    test bugfix branch

--- main.tf
+++ main.tf
@@ -9,31 +9,52 @@ provider "google" {
 }
 
 // Networks
-resource "google_compute_network" "default" {
-  name = "default-${var.env}"
+resource "google_compute_network" "network-1" {
+  name = "network-1-${var.env}"
 }
 
 //Subnetworks
 
-resource "google_compute_subnetwork" "default" {
-  name   = "default-${var.env}"
+resource "google_compute_subnetwork" "subnet-1-a" {
+  name   = "subnet-1-a-${var.env}"
   region = "europe-west1"
-  ip_cidr_range = "10.0.0.0/24"
-  network = "${google_compute_network.default.name}"
+  ip_cidr_range = "10.0.1.0/24"
+  network = "${google_compute_network.network-1.name}"
+}
+
+resource "google_compute_subnetwork" "subnet-1-b" {
+  name   = "subnet-1-b-${var.env}"
+  region = "europe-west1"
+  ip_cidr_range = "10.0.2.0/24"
+  network = "${google_compute_network.network-1.name}"
 }
 
 //Instance
 
-resource "google_compute_instance" "default-img" {
-  name         = "default-img-${var.env}"
+resource "google_compute_instance" "instance-1-a-1" {
+  name         = "instance-1-a-1-${var.env}"
   machine_type = "f1-micro"
   zone         = "europe-west1-b"
-  tags = ["default"]
+  tags = ["instance-1-a-1"]
   disk {
     image = "centos-cloud/centos-7"
   }
   network_interface {
-    subnetwork = "${google_compute_subnetwork.default.name}"
+    subnetwork = "${google_compute_subnetwork.subnet-1-a.name}"
+  //  access_config = "${google_compute_address.default.name}"
+  }
+}
+
+resource "google_compute_instance" "instance-1-b-1" {
+  name         = "instance-1-b-1-${var.env}"
+  machine_type = "f1-micro"
+  zone         = "europe-west1-b"
+  tags = ["instance-1-b-1"]
+  disk {
+    image = "centos-cloud/centos-7"
+  }
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.subnet-1-b.name}"
   //  access_config = "${google_compute_address.default.name}"
   }
 }
